76a6408ae005cd22af79a3fb963828372be50f60
Jonghan Park <jonghan.park@edu.sait.ca>
2022-07-03 20:23:48 -0600

    Update service

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from "react";
+import service_data from "../data/service_data";
 
 export default function Services() {
   return (
@@ -8,24 +9,30 @@ export default function Services() {
           <div className="row">
             <div className="col-12">
               <h3 className="fs-5 text-center mb-0">Our Services</h3>
-              <h1 className="display-6 text-center mb-4">Our <b>Awesome</b> Services</h1>
-              <hr className='w-25 mx-auto' />
+              <h1 className="display-6 text-center mb-4">
+                Our <b>Awesome</b> Services
+              </h1>
+              <hr className="w-25 mx-auto" />
             </div>
           </div>
-          <div className="row">
-            <div className="col-md-4">
-              <div className="card">
-                <img src="..." className="card-img-top" alt="..." />
-                <div className="card-body">
-                  <h5 className="card-title">Card title</h5>
-                  <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                  <a href="#" className="btn btn-primary">Go somewhere</a>
+          <div className="row mt-5">
+            {service_data.map((service) => {
+              const { id, class_name, title, contents } = service;
+              return (
+                <div className="col-md-4" key={id}>
+                  <div className="card p-3">
+                    <div className="card-body text-center">
+                      <i className={class_name}></i>
+                      <h5 className="card-title mb-3 fs-4 fw-bold">{title}</h5>
+                      <p className="card-text lead">{contents}</p>
+                    </div>
+                  </div>
                 </div>
-            </div>
-            </div>
+              );
+            })}
           </div>
         </div>
       </section>
     </div>
-  )
+  );
 }
---
+++ src/data/service_data.js
@@ -0,0 +1,46 @@
+const service_data = [
+  {
+    id: 1,
+    class_name: "fa fa-cogs fa-4x mb-4 text-primary",
+    title: "Highly Customizable",
+    contents:
+      "Some quick example text to build on the card title and make up the bulk of the cards content.",
+  },
+  {
+    id: 2,
+    class_name: "fa fa-mobile fa-4x mb-4 text-primary",
+    title: "Fully Responsive Layout",
+    contents:
+      "Some quick example text to build on the card title and make up the bulk of the cards content.",
+  },
+  {
+    id: 3,
+    class_name: "fa fa-users fa-4x mb-4 text-primary",
+    title: "Users Experience",
+    contents:
+      "Some quick example text to build on the card title and make up the bulk of the cards content.",
+  },
+  {
+    id: 4,
+    class_name: "fa fa-laptop fa-4x mb-4 text-primary",
+    title: "Creative Web Design",
+    contents:
+      "Some quick example text to build on the card title and make up the bulk of the cards content.",
+  },
+  {
+    id: 5,
+    class_name: "fa fa-file-code-o fa-4x mb-4 text-primary",
+    title: "Unique and Clean",
+    contents:
+      "Some quick example text to build on the card title and make up the bulk of the cards content.",
+  },
+  {
+    id: 6,
+    class_name: "fa fa-star-half-o fa-4x mb-4 text-primary",
+    title: "Creative Ideas",
+    contents:
+      "Some quick example text to build on the card title and make up the bulk of the cards content.",
+  },
+];
+
+export default service_data;
