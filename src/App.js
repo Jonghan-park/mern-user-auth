a66b1fa58daf6d0991b3a749c25923c2553e6bc0
Jonghan Park <jonghan.park@edu.sait.ca>
2022-07-03 20:43:35 -0600

    Update contact

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import './App.css';
-import Navbar from './components/Navbar'
-import Home from './components/Home'
-import About from './components/About'
-import Services from './components/Services'
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
+import Services from "./components/Services";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
       <Home />
       <About />
       <Services />
+      <Contact />
+      <Footer />
     </>
   );
 }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,65 @@
 import React from "react";
 
 export default function Contact() {
-  return <div>Contact</div>;
+  return (
+    <div>
+      <section id="contact">
+        <div className="container my-5 py-5">
+          <div className="row mb-5">
+            <div className="col-12">
+              <h3 className="fs-5 text-center mb-0">Contact Us</h3>
+              <h1 className="display-6 text-center mb-4">
+                Have Some <b>Question?</b>
+              </h1>
+              <hr className="w-25 mx-auto" />
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-6">
+              <img src="/assets/contact.png" alt="contact" className="w-75" />
+            </div>
+            <div className="col-md-6">
+              <form action="">
+                <div class="mb-3">
+                  <label for="name" className="form-label">
+                    Your Name
+                  </label>
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="name"
+                    placeholder="John Smith"
+                  />
+                </div>
+                <div class="mb-3">
+                  <label for="email" className="form-label">
+                    Email Address
+                  </label>
+                  <input
+                    type="email"
+                    class="form-control"
+                    id="email"
+                    placeholder="name@example.com"
+                  />
+                </div>
+                <div className="mb-3">
+                  <label for="message" className="form-label">
+                    Your Message
+                  </label>
+                  <textarea
+                    className="form-control"
+                    id="message"
+                    rows="5"
+                  ></textarea>
+                </div>
+                <button type="submit" className="btn btn-outline-primary">
+                  Send Message<i className="fa fa-paper-plane ms-2 "></i>
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 }
