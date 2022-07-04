e21c0655b9b1314e88ef338f5797f088fc5484d7
Jonghan Park <jonghan.park@edu.sait.ca>
2022-07-03 23:41:26 -0600

    Update login form

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Home from "./components/Home";
 import About from "./components/About";
 import Services from "./components/Services";
 import Contact from "./components/Contact";
+import Login from "./components/Login";
+import Register from "./components/Register";
 import Footer from "./components/Footer";
 import { Route, Routes } from "react-router-dom";
 
@@ -16,6 +18,8 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/service" element={<Services />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
       <Footer />
     </>
