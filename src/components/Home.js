6654b3666d1f0c2d4fe68e62fa567b39e319ea2d
Jonghan Park <jonghan.park@edu.sait.ca>
2022-07-03 19:12:33 -0600

    Create Home

---
+++ src/components/Home.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div>
+      <section className="home">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8">
+              <h1 className="display-4">Feels the Fresh Business Perspective</h1>
+              <p className="lead">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum voluptatem velit beatae necessitatibus accusamus maxime voluptatum molestiae quisquam amet totam! Tenetur voluptate nulla magnam adipisci iste, ipsa asperiores culpa quidem.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  )
+}
