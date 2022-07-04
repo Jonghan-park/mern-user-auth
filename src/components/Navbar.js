0305fabee68369b483ee17d49fcb3415298c6a20
Jonghan Park <jonghan.park@edu.sait.ca>
2022-07-03 19:45:14 -0600

    Update services

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,28 +3,28 @@ import React from 'react'
 export default function Navbar() {
   return (
     <div>
-      <nav class="navbar navbar-expand-lg shadow">
-  <div class="container">
+      <nav className="navbar navbar-expand-lg shadow">
+  <div className="container">
     
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">Home</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">About</a>
+        <li className="nav-item">
+          <a className="nav-link" href="#">About</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Services</a>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Services</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Contact</a>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Contact</a>
         </li>
       </ul>
-      <a class="navbar-brand fw-bolder fs-4 mx-auto" href="#">Navbar</a>
+      <a className="navbar-brand fw-bolder fs-4 mx-auto" href="#">Navbar</a>
       <button className='btn btn-outline-primary ms-auto px-4 rounded-pill'><i className="fa fa-sign-in me-2"></i> Login</button>
       <button className='btn btn-outline-primary ms-2 px-4 rounded-pill'><i className="fa fa-user-plus me-2"></i>Register</button>
     </div>
