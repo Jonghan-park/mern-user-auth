0c39b7f85ac9d39baa1c3527483672b1a5ce5625
Jonghan Park <jonghan.park@edu.sait.ca>
2022-07-05 00:36:27 -0600

    Update login api

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,7 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
+import { useNavigate } from "react-router";
 
 export default function Register() {
+  const navigate = useNavigate();
+
+  const [user, setUser] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  // Handle Inputs
+  const handleInput = (e) => {
+    let name = e.target.name;
+    let value = e.target.value;
+
+    setUser({ ...user, [name]: value });
+  };
+
+  // Handle Submit
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    // Object Destructuring
+    // Store Object Data into Variables
+    const { username, email, password } = user;
+    try {
+      // It is Submitted on port 3000 by default
+      // Which is front end but we need to submit it on backend
+      // which is on port 5000. so we need proxy
+      const res = await fetch("/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username,
+          email,
+          password,
+        }),
+      });
+
+      if (res.status === 400 || !res) {
+        window.alert("Already Used Details");
+      } else {
+        window.alert("Registered Successfully");
+        // Use useNavigate instead of using useHistory
+        navigate("/login");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div>
       <div className="container shadow my-5">
@@ -20,24 +71,43 @@ export default function Register() {
           <div className="col-md-6 p-5">
             <h1 className="display-6 fw-bolder mb-5">Register</h1>
 
-            <form>
+            <form onSubmit={handleSubmit} method="POST">
               <div className="mb-3">
                 <label for="name" className="form-label">
                   Username
                 </label>
-                <input type="text" className="form-control" id="name" />
+                <input
+                  type="text"
+                  className="form-control"
+                  id="name"
+                  name="username"
+                  value={user.username}
+                  onChange={handleInput}
+                />
               </div>
               <div className="mb-3">
                 <label for="email" className="form-label">
                   Email
                 </label>
-                <input type="email" className="form-control" id="email" />
+                <input
+                  type="email"
+                  className="form-control"
+                  id="email"
+                  name="email"
+                  value={user.email}
+                />
               </div>
               <div className="mb-3">
                 <label for="password" className="form-label">
                   Password
                 </label>
-                <input type="password" className="form-control" id="password" />
+                <input
+                  type="password"
+                  className="form-control"
+                  id="password"
+                  name="password"
+                  value={user.password}
+                />
               </div>
               <div className="mb-3 form-check">
                 <input
