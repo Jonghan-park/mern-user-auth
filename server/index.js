70c530246b3de2e0bc802978b19d025a210016ad
Jonghan Park <jonghan.park@edu.sait.ca>
2022-07-05 00:08:32 -0600

    Update login api

--- server/index.js
+++ server/index.js
@@ -3,16 +3,21 @@ const dotenv = require("dotenv");
 const bcryptjs = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const cookieParser = require("cookie-parser");
-const port = process.env.PORT || 5000;
 
 const app = express();
 
 // Configure ENV file & Require Connection File
 dotenv.config({ path: "./.env" });
 require("./db/conn");
+const port = process.env.PORT || 5000;
 
 //Require Model
 const Users = require("./models/userSchema");
+
+// These method is used to get data and cookies from frondend
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("Hello World");
@@ -32,13 +37,38 @@ app.post("/register", async (req, res) => {
       password: password,
     });
 
-    // Save Method is used to create user
+    // Save Method is used to create user or insert user
+    // But before saving or inserting, password will hash
+    // Because of hashing. After hash, it will save to DB
     const created = await createUser.save();
+    console.log(created);
+    res.status(200).send("Registered");
   } catch (error) {
-    console.log(error);
+    res.status(400).send(error);
   }
 });
 
+//Login User
+app.post("/login", async (req, res) => {
+  try {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    //Find User if exist
+    const user = await Users.findOne({ email: email });
+    if (user) {
+      // Verify password
+      const isMatch = await bcryptjs.compare(password, users.password);
+
+      if (isMatch) {
+        // Generate Token which is define in user Schema
+        const token = await user.generateToken();
+      }
+    }
+  } catch (error) {}
+});
+
+//Run server
 app.listen(port, () => {
   console.log(`App is listening on port ${port}`);
 });
