adf439c49b1e94a4d7f45a7550b078206142d1fa
Jonghan Park <jonghan.park@edu.sait.ca>
2022-07-04 23:36:17 -0600

    Update register

--- server/index.js
+++ server/index.js
@@ -11,8 +11,32 @@ const app = express();
 dotenv.config({ path: "./.env" });
 require("./db/conn");
 
+//Require Model
+const Users = require("./models/userSchema");
+
 app.get("/", (req, res) => {
   res.send("Hello World");
+});
+
+//Registration
+app.post("/register", async (req, res) => {
+  try {
+    //Get body or data
+    const username = req.body.username;
+    const email = req.body.email;
+    const password = req.body.password;
+
+    const createUser = new Users({
+      username: username,
+      email: email,
+      password: password,
+    });
+
+    // Save Method is used to create user
+    const created = await createUser.save();
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 app.listen(port, () => {
